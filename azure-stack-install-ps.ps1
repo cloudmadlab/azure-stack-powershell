4022147601ff51608617e9cffb71fb198abe1709
Michael Lamia <michael.a.lamia@gmail.com>
2020-01-31 08:41:31 -0600

    Correct type-o in script

--- azure-stack-install-ps.ps1
+++ azure-stack-install-ps.ps1
@@ -2,11 +2,11 @@ Start-Process powershell -Verb runAs
 
 Find-PackageProvider -Name 'Nuget' -ForceBootstrap -IncludeDependencies
 
-Import-Module -Name PowerShellGet -ErrorActiStop
+Import-Module -Name PowerShellGet -ErrorActionStop
 Import-Module -Name PackageManagement -ErrorAction Stop
 Get-PSRepository -Name "PSGallery"
 
-Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted 
+Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 Install-Module -Name AzureRM.BootStrapper -Force
 
 Get-AzureRMProfile -Update
@@ -15,6 +15,6 @@ Install-Module -Name AzureStack -RequiredVersion 1.7.2
 
 cd \
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-invoke-webrequest https://github.com/Azure/AzureStack-Tools/archive/master.zip -OutFile master.zip
+invoke-webrequest https://github.com/Azure/AzureStack-Tools/archive/master.zip -OutFile master.zip 
 expand-archive master.zip -DestinationPath . -Force
 cd AzureStack-Tools-master
